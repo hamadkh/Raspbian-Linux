f1a0bfce17637cae87fed7d7a0fde57d264ea82c
hamad Khawaja <khawajahamad217@gmail.com>
2017-02-02 09:35:39 -0800

    Add files via upload

---
+++ cmdargv.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+        int i = 0;
+        for(i=argc-1; i>0; i--)
+        {
+           printf("%s ",argv[i] );
+        }
+        printf("\n");
+
+}
