f20469a4d169b1583915e377332092b8261b892c
hamad Khawaja <khawajahamad217@gmail.com>
2017-03-15 15:59:06 -0400

    Update ls2.c

--- ls2.c
+++ ls2.c
@@ -1,176 +1,215 @@
-/** ls2.c
- **/
-#include    <stdio.h>
-#include    <math.h>
-#include    <sys/types.h>
-#include    <dirent.h>
-#include    <stdlib.h>
-#include    <sys/ioctl.h>
-#include    <string.h>
+// Hamad Khawaja
+// myls
+// -a
+// -r
+// -s
+// CS355
 
-void do_ls(char []);
+
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+
+
+#define row 100
+#define col 100
+
+int cmp(char **a, char **b);
+void printSpaces(char* str,int maxstrlen);
 struct winsize *get_screen_dimensions();
-int compare(const void *str1, const void *str2);
-int get_longest_word(char **array, int arrlen);
-void addspaces(char *str, int maxstrlen);
+int longest_word(char **array, int arrlen);
 void setup_2d_array(int **arr, int numrows, int numcols);
 
-int main(int ac, char *av[])
-{
-    if (ac == 1)
-        do_ls(".");
-    else
-    {
-        while (--ac)
-        {
-            do_ls(*av);
-        }
-    }
+
+int cmp (char **a, char **b){
+return strcmp(* a,*b);
 }
 
-/*
- *	list files in directory called dirname
- */
-void do_ls( char dirname[] )
+int printInfo(DIR *d, int dHiddens,int order,int rorder)
 {
-    DIR     *dir_ptr;		/* the directory */
-    struct  dirent	*direntp;		/* each entry	 */
-    char    **dirArray = (char **)malloc(sizeof(char *) * 512);
-    int     dirArrayLen = 0;
-    
-	if ((dir_ptr = opendir(dirname)) == NULL)
-        fprintf(stderr,"ls1: cannot open %s\n", dirname);
-    else
+    int count;
+    int i=0,j=0,k;
+    unsigned char **p=NULL;
+    int isize=0;
+    char **dirArray= (char**)malloc(sizeof(char*)*512);
+    int dirArraylen=0;
+    int index1;
+    int row2,col2;	
+    p=malloc(row * sizeof (unsigned char *));
+
+    if(d==NULL){ printf("Could not read directory!\n"); return; }
+    struct dirent *direct;
+
+    while((direct = readdir(d))!=NULL)
     {
-        while ((direntp = readdir(dir_ptr)) != NULL)
-        {
-            if (direntp->d_name[0] == '.')
-                continue;
-            dirArray[dirArrayLen] = direntp->d_name; 
-            dirArrayLen++;
-        }
-        closedir(dir_ptr);
+        if(direct<0)
+	{ printf("Cant open folder"); return;}
+        if(direct->d_name[0]!='.'||dHiddens)
+	{
+		if(order==0 && rorder==0)
+		{
+			//printf("%s",direct->d_name);
+		}
+		dirArray[dirArraylen]=direct->d_name;
+		dirArraylen++;
+       		p[i]= malloc (col * sizeof (unsigned char));
+		strcpy(p[i],direct->d_name);
+        	i++;
+  	}
     }
+
+	int stringLen=sizeof(dirArray)/sizeof(char*);
+	int longestWordLen= longest_word(dirArray,dirArraylen); //function call to find longest word	
+
+	
+    	struct winsize *winset = get_screen_dimensions();
+    	unsigned short scrnwdth = winset->ws_col;
    
-    /* Get the size of the window */
-    struct winsize *winset = get_screen_dimensions();
-    /* From the winset struct, get the width of the window in columns */
-    unsigned short scrnwdth = winset->ws_col;
-    /* Get the size of each string */
-    int stringLen = sizeof(dirArray) / sizeof(char *);
-    /* Sort the array using the qsort() library function */
-    qsort(dirArray, stringLen, sizeof(char *), compare);
-    /* Find the longest word in the array (as this will be used as reference for printing */
-    int longestwordlen = get_longest_word(dirArray, dirArrayLen);
 
-    /* Get the number of columns */
-    int numcols = floor(((double)scrnwdth / (longestwordlen + 1)));
-    /* Get the number of rows */
-    int numrows = ceil(((double)dirArrayLen / numcols));
-   
-    /* Create 2D Array */
-    /* This array will store the indices of each string */
-    int **print_dirs;
-    /* Allocate memory to the pointers */
-    print_dirs = (int **)malloc(numrows * sizeof(*print_dirs));
-    for (int index = 0; index < numrows; index++)
-        print_dirs[index] = (int *)malloc(numcols * sizeof(*print_dirs[index]));
-    /* Fill in their respective values */
-    setup_2d_array(print_dirs, numrows, numcols);
+    	int numcols = floor(((double)scrnwdth / (longestWordLen + 1)));
+    	int numrows = ceil(((double)dirArraylen / numcols));
+	   
+	int **print_dirs;
+	
+    	print_dirs = (int **)malloc(numrows * sizeof(*print_dirs));
+    	for (index1 = 0; index1 < numrows; index1++)
+        	print_dirs[index1] = (int *)malloc(numcols * sizeof(*print_dirs[index1]));
 
-    /* Loop through the matrix, printing each directory, correctly spaced and alphabetically */
-    for (int row = 0; row < numrows; row++)
-    {
-        for (int col = 0; col < numcols; col++)
-        {
-            /* If the index is out of bounds, do not print it */
-            if (print_dirs[row][col] > (dirArrayLen - 1))
-                continue;
-            else
-            {
-                addspaces(dirArray[print_dirs[row][col]], longestwordlen);
-                printf("%s", dirArray[print_dirs[row][col]]);
-            }
-        }
-        /* Print a new line to move to the next line */
-        printf("\n");
-    }
-    /* Free memory from leaks */
+   	 setup_2d_array(print_dirs, numrows, numcols);
+	 
+    	for (row2 = 0; row2 < numrows; row2++)
+    	{
+        	for (col2 = 0; col2 < numcols; col2++)
+        	{
 
-    for (int index = 0; index < numrows; index++)
-        free(print_dirs[index]);
-    free(print_dirs);
-    free(dirArray);
-    free(winset);
+            		if (print_dirs[row2][col2] > (dirArraylen - 1))
+                		continue;
+            		else
+            		{
+                		printSpaces(dirArray[print_dirs[row2][col2]], longestWordLen);
+                		printf("%s", dirArray[print_dirs[row2][col2]]);
+            		}
+        	}
+
+    	}
+	printf("\n");
+	free(p);
+	free(dirArray);
+	free(print_dirs);
+	free(winset);
 }
 
-/*
- * Sets up the 2D array of indices
- */
 void setup_2d_array(int **arr, int numrows, int numcols)
 {
     /* When looping, this variable represents each index of dirArray */
     int dirIndex = 0;
+	int row1,col1;
     /* Loop through each row/col in the matrix, and add the indices */
-    for (int col = 0; col < numcols; col++)
+    for (col1 = 0; col1 < numcols; col1++)
     {
-        for (int row = 0; row < numrows; row++)
+        for (row1 = 0; row1 < numrows; row1++)
         {
-            arr[row][col] = dirIndex;
+            arr[row1][col1] = dirIndex;
             dirIndex++;
         }
     }
 }
-
-/*
- * Adds a number of spaces to the end of a string to get it
- * to a certain length
- */
-void addspaces(char *str, int maxstrlen)
+	
+void printSpaces(char* str, int maxstrlen)
 {
-   /* Get the current string length */
-   size_t curr_strlen = strlen(str);
-
-   /* Get how many spaces would need to be added */
-   int numspcs = maxstrlen - curr_strlen;
-
-   /* Add the spaces */
-   for (int index = 0; index <= numspcs; index++)
-   {
-        strcat(str, " ");
-   }
-}
-/*
- * Compares the strings and returns their result using strcmp
- */
-int compare(const void *str1, const void *str2)
-{
-    const char *istr1 = (const char *)str1;
-    const char *istr2 = (const char *)str2;
-    return strcmp(istr1, istr2);
-}
-/*
- * Returns the length of the longest word
- */
-int get_longest_word(char **array, int arrlen)
-{
-    size_t longestlen = 0;
-    for (int index = 0; index < arrlen; index++)
-    {
-        /* Get string length */
-        size_t currstrlen = strlen(array[index]);
-        if (currstrlen > longestlen)
-            longestlen = currstrlen;
-    }
-    return longestlen;
+	size_t currstrlen= strlen(str);
+	int numspaces=maxstrlen - currstrlen;
+	int i=0;
+	for(;i<=numspaces;i++)
+	{
+		strcat(str," ");
+	}
 }
 
-/*
- * Creates a winsize struct with the screen dimensions
- */
+// Calculates the longest file name in the directory
+
+int longest_word(char** arr, int arrlen)
+{
+	size_t longestlen=0;
+	int index=0;
+	for(;index<arrlen;index++)
+	{
+		size_t currstrlen= strlen(arr[index]);
+		if(currstrlen > longestlen)
+		longestlen=currstrlen;
+	}
+	//printf("*%d*",longestlen);
+	return longestlen;
+}
+
 struct winsize *get_screen_dimensions()
 {
     struct winsize *wbuf = (struct winsize *)malloc(sizeof(struct winsize));
     ioctl(0, TIOCGWINSZ, wbuf);
     return wbuf;
+}
+
+
+int main(int argc, char* argv[])
+{
+    DIR *dir;
+    int a = 0;
+    int argCount = argc;
+    int displayHiddens = 0;
+    int rorder = 0;
+    int order = 0;
+
+
+    while((a = getopt(argc, argv, "ars")) != -1) {
+	switch(a) {
+	case 'a':
+	displayHiddens=1;
+	break;
+	case 'r':
+	rorder=1;
+	break;
+	case 's':
+	order=1;
+	break;
+	default:
+	exit(EXIT_FAILURE);
+	}
+     }
+
+	//if check current file directory
+	int k;
+	for(k=0;k<argc;k++)
+	{
+		if((strcmp(argv[k],"-as")==0)|| (strcmp(argv[k],"-a")==0)|| (strcmp(argv[k],"-r")==0) || (strcmp(argv[k],"-s")==0)|| strcmp(argv[k],"-ar")==0)
+		{
+		    argCount--;
+			}
+	}
+
+	if(argCount >=2)
+	{
+	int x;
+
+	for(x=1;x<argc;x++)
+	{
+		if(argv[x][0]=='-') continue;
+		dir=opendir(argv[x]);
+		printInfo(dir, displayHiddens,order,rorder);
+		closedir(dir);
+		}
+	}
+	else
+	{
+	dir= opendir(".");
+	printInfo(dir, displayHiddens,order,rorder);
+	closedir(dir);
+	}
+
+    return 0;
 }
